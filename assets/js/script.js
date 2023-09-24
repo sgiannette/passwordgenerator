21a7c1c4811553ca0ab585170cf317f98066cbef
Sabrina Giannette <sgiannette5@gmail.com>
2023-09-23 18:58:32 -0700

    added read.me

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,38 @@
-// Assignment Code
+
 var lowerCase = "abcdefghijklmnopqrstuvwxyz"
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numeric = "1234567890"
 var specialCharacters = "!@#$%^&*?"
-var finalPassword = ""
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
+  var finalPassword = ""
   var passwordSize = prompt("Please enter password length within 8 and 128 characters")
   if (passwordSize >= 8 && passwordSize <= 128) {
     var confirmLowercase = confirm("Do you want lowercase letters?")
     var confirmUppercase = confirm("Do you want an uppercase letter?")
     var confirmNumeric = confirm("Do you want to use a number?")
     var confirmSpecialcharacters = confirm("Do you want to use a special character?")
-    if (confirmLowercase === true) {
-      var index = Math.floor(Math.random() * 26)
-      finalPassword = finalPassword + lowerCase[index]
+    if (confirmLowercase === false && confirmUppercase === false && confirmNumeric === false && confirmSpecialcharacters === false){
+      alert("You must choose one password type!")
+      return ""
     }
-    if (confirmUppercase === true) {
-      var index = Math.floor(Math.random()*26)
-      finalPassword = finalPassword + upperCase[index]
-    }
-    if (confirmNumeric === true) {
-      var index = Math.floor(Math.random()*10)
-      finalPassword = finalPassword + numeric[index]
-    }
-    if (confirmSpecialcharacters === true){
-      var index = Math.floor(Math.random()*9)
-      finalPassword = finalPassword + specialCharacters[index]
+    for (let i = 0; i < passwordSize; i++) {
+      if (confirmLowercase === true && finalPassword.length<passwordSize) {
+        var index = Math.floor(Math.random() * 26)
+        finalPassword = finalPassword + lowerCase[index]
+      }
+      if (confirmUppercase === true && finalPassword.length<passwordSize) {
+        var index = Math.floor(Math.random() * 26)
+        finalPassword = finalPassword + upperCase[index]
+      }
+      if (confirmNumeric === true && finalPassword.length<passwordSize) {
+        var index = Math.floor(Math.random() * 10)
+        finalPassword = finalPassword + numeric[index]
+      }
+      if (confirmSpecialcharacters === true && finalPassword.length<passwordSize) {
+        var index = Math.floor(Math.random() * 9)
+        finalPassword = finalPassword + specialCharacters[index]
+      }
     }
   }
   else {
